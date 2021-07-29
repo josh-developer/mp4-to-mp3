5b92ca440dff4b2a956789017825b0f11a35e230
MOSCOW\Jushkinbek_Sadullaev <Jushkinbek_Sadullaev@epam.com>
2021-07-29 20:48:36 +0500

    done

--- main.js
+++ main.js
@@ -1,42 +1,70 @@
-import {convert} from './vid.js';
+import { convert } from './vid.js';
 
 
-
-let input = document.querySelector("#inp");
-let loader =  document.querySelector("#loading");
-let a =  document.querySelector("#link");
+let input = getEl("#inp");
+let loader = getEl("#loading");
+let a = getEl("#link");
+let audio_name_box = getEl("#name-box");
+let audio_name = getEl("#name");
 
 
 async function convertToAudio() {
-    loader.classList.remove("d-none");
-    loader.classList.add('d-block')
+    addClass(loader, 'd-block');
+    removeClass(loader, 'd-none');
+
     let sourceVideoFile = input.files[0];
     let targetAudioFormat = 'mp3'
     let convertedAudioDataObj = await convert(sourceVideoFile, targetAudioFormat);
 
-    downloadAudio(convertedAudioDataObj)
+    downloadAudio(convertedAudioDataObj);
 }
 
 
 function downloadAudio(convertedAudioDataObj) {
-    let name =  document.querySelector("#name");
-    name.value = convertedAudioDataObj.name;
-    
-    name.classList.remove("d-none");
-    name.classList.add("d-block");
-    loading.classList.remove("d-block");
-    loading.classList.add('d-none');
-    a.classList.remove('d-none');
-    a.classList.add('d-flex');
+    audio_name.value = convertedAudioDataObj.name;
+
+    toggleClassBetween(audio_name_box, 'd-block', 'd-none')
+    toggleClassBetween(loading, 'd-none', 'd-block')
+    toggleClassBetween(a, 'd-flex', 'd-none')
 
     a.textContent = "Download " + convertedAudioDataObj.format + " version";
     a.href = convertedAudioDataObj.data;
-   
-    a.addEventListener('click', _ =>{       
-        convertedAudioDataObj.name = name.value;
+
+    a.addEventListener('click', _ => {
+        convertedAudioDataObj.name = audio_name.value;
         a.download = convertedAudioDataObj.name + "." + convertedAudioDataObj.format;
+
+        setTimeout(() => {
+            toggleClassBetween(a, 'd-none', 'd-flex')
+            toggleClassBetween(audio_name_box, 'd-none', 'd-flex')
+
+            input.value = ''
+            audio_name.value = ''
+
+        }, 100);
     })
- 
+
 }
 
 inp.addEventListener('change', convertToAudio, this);
+
+
+
+
+// helper functions
+
+function addClass(target, cl) {
+    target.classList.add(cl);
+}
+
+function removeClass(target, cl) {
+    target.classList.remove(cl);
+}
+function toggleClassBetween(target, add, remove) {
+    addClass(target, add);
+    removeClass(target, remove);
+}
+
+function getEl(by) {
+    return document.querySelector(by);
+}
